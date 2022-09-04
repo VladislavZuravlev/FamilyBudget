4821d4a72d219cffcf1e4df5f5c59c782137b28d
zuravlevvladislav19@gmail.com <zuravlevvladislav19@gmail.com>
2022-09-04 17:49:28 +0300

    Добавьте файлы проекта.

---
+++ FamilyBudget/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FamilyBudget.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
